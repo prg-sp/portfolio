fc3d9039059b68a78d7dc716989acbad6588cfed
prg-sp <wdev.prg.sp@gmail.com>
2022-02-24 02:01:01 +0200

    image-webpack-loader (foto kompresorius)

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,7 @@
 const path = require('path');
+//5.
+// const ImageminPlugin = require('imagemin-webpack-plugin').default;
+// const glob = require('glob');
 
 module.exports = {
 	entry: ['./src/js/main.js', './src/css/stilizmas.css'],
@@ -6,6 +9,17 @@ module.exports = {
 	// 	supercool: './src/js/main.js',
 	// 	stiliuskas: './src/css/stilizmas.css',
 	// },
+
+	// plugins: [
+	// 	new ImageminPlugin({
+	// 		externalImages: {
+	// 			context: '.',
+	// 			source: glob.sync('src/assets/**/*.{png,jpg,jpeg,gif,svg}'),
+	// 			destination: 'dist/foo',
+	// 			filename: '[name].[ext]',
+	// 		},
+	// 	}),
+	// ],
 
 	module: {
 		rules: [
@@ -16,7 +30,6 @@ module.exports = {
 					attrs: ['img:src', ':srcset'],
 				},
 			},
-
 			{
 				test: /\.(svg|png|jpg|gif|webp)$/,
 				exclude: [/node_modules/],
@@ -30,6 +43,38 @@ module.exports = {
 				},
 				type: 'javascript/auto',
 			},
+
+			{
+				loader: 'image-webpack-loader',
+				options: {
+					bypassOnDebug: true, // webpack@1.x
+					disable: true, // webpack@2.x and newer
+				},
+			},
+
+			// {
+			// 	loader: 'image-webpack-loader',
+			// 	options: {
+			// 		mozjpeg: {
+			// 			progressive: true,
+			// 		},
+			// 		// optipng.enabled: false will disable optipng
+			// 		optipng: {
+			// 			enabled: false,
+			// 		},
+			// 		pngquant: {
+			// 			quality: [0.65, 0.9],
+			// 			speed: 4,
+			// 		},
+			// 		gifsicle: {
+			// 			interlaced: false,
+			// 		},
+			// 		// the webp option will enable WEBP
+			// 		webp: {
+			// 			quality: 75,
+			// 		},
+			// 	},
+			// },
 		],
 	},
 };
